6401558ae14d7ddf1c06d6d477ab96d6d4a3556c
Glenn-Rhee <glennviktor5@gmail.com>
2025-02-01 00:32:49 +0700

    feat: fix usesearchparams

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,67 +12,70 @@ import {
 } from "./ui/dropdown-menu";
 import SearchProduct from "./SearchProduct";
 import Container from "./Container";
+import { Suspense } from "react";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 z-50 right-0 left-0 px-4 py-2 md:px-10 md:py-5 bg-[#1B6392]">
-      <Container className="flex items-center justify-between mt-0 md:mt-0 lg:mt-0">
-        <Link
-          href={"/"}
-          className="text-white flex items-center gap-x-1 lg:gap-x-3"
-        >
-          <Image
-            src={"/letter-a.png"}
-            alt="icon name"
-            width={25}
-            height={25}
-            className="w-auto aspect-square"
-          />
-          <span className="text-base md:text-3xl font-bold uppercase">
-            Addorama
-          </span>
-        </Link>
-        <SearchProduct className="" />
-        <div className="lg:flex hidden items-center gap-x-7 text-white">
-          {/* <ShoppingCart size={25} className="cursor-pointer" /> */}
-          <Link href={"/cart"}>
-            <CartFill size={25} />
+    <Suspense>
+      <nav className="fixed top-0 z-50 right-0 left-0 px-4 py-2 md:px-10 md:py-5 bg-[#1B6392]">
+        <Container className="flex items-center justify-between mt-0 md:mt-0 lg:mt-0">
+          <Link
+            href={"/"}
+            className="text-white flex items-center gap-x-1 lg:gap-x-3"
+          >
+            <Image
+              src={"/letter-a.png"}
+              alt="icon name"
+              width={25}
+              height={25}
+              className="w-auto aspect-square"
+            />
+            <span className="text-base md:text-3xl font-bold uppercase">
+              Addorama
+            </span>
           </Link>
-          <Link href={"/favorite"}>
-            <Heart size={25} className="cursor-pointer" />
-          </Link>
-          <Link href={"/profile"}>
-            <User size={25} className="cursor-pointer" />
-          </Link>
-        </div>
-        <div className="block lg:hidden">
-          <DropdownMenu>
-            <DropdownMenuTrigger>
-              <Menu color="white" />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuGroup>
-                <DropdownMenuItem className="flex items-center gap-x-3">
-                  <CartFill size={18} />
-                  <Link href={"/cart"} className="text-sm font-semibold">
-                    Cart
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="flex items-center gap-x-3">
-                  <Heart size={25} className="cursor-pointer" />
-                  <Link href={"/favorite"} className="text-sm font-semibold">
-                    Favorite
-                  </Link>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="flex items-center gap-x-3">
-                  <UserRound size={25} />
-                  <span className="text-sm font-semibold">Account</span>
-                </DropdownMenuItem>
-              </DropdownMenuGroup>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
-      </Container>
-    </nav>
+          <SearchProduct className="" />
+          <div className="lg:flex hidden items-center gap-x-7 text-white">
+            {/* <ShoppingCart size={25} className="cursor-pointer" /> */}
+            <Link href={"/cart"}>
+              <CartFill size={25} />
+            </Link>
+            <Link href={"/favorite"}>
+              <Heart size={25} className="cursor-pointer" />
+            </Link>
+            <Link href={"/profile"}>
+              <User size={25} className="cursor-pointer" />
+            </Link>
+          </div>
+          <div className="block lg:hidden">
+            <DropdownMenu>
+              <DropdownMenuTrigger>
+                <Menu color="white" />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuGroup>
+                  <DropdownMenuItem className="flex items-center gap-x-3">
+                    <CartFill size={18} />
+                    <Link href={"/cart"} className="text-sm font-semibold">
+                      Cart
+                    </Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="flex items-center gap-x-3">
+                    <Heart size={25} className="cursor-pointer" />
+                    <Link href={"/favorite"} className="text-sm font-semibold">
+                      Favorite
+                    </Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem className="flex items-center gap-x-3">
+                    <UserRound size={25} />
+                    <span className="text-sm font-semibold">Account</span>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </Container>
+      </nav>
+    </Suspense>
   );
 }
